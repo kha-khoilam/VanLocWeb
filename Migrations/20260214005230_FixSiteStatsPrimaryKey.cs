d74a8b9e423cbf6ba9ded3f98d8b4ef44c846e51
Khôi Lâm <khoilam1111@gmail.com>
2026-02-14 07:58:00 +0700

    Update Database Schema

---
+++ Migrations/20260214005230_FixSiteStatsPrimaryKey.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VanLocWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSiteStatsPrimaryKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SiteStats",
+                table: "SiteStats");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "SiteStats",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalVisits",
+                table: "SiteStats",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SiteStats",
+                table: "SiteStats",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SiteStats",
+                table: "SiteStats");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalVisits",
+                table: "SiteStats",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "SiteStats",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SiteStats",
+                table: "SiteStats",
+                column: "TotalVisits");
+        }
+    }
+}
